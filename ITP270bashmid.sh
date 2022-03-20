7040ace857111c6f9374b6820564a66d830293d3
ALilalee <ali.lalee@yahoo.com>
2022-03-20 19:26:13 -0400

    bash

--- ITP270bashmid.sh
+++ ITP270bashmid.sh
@@ -12,25 +12,28 @@
 # - Upload the script in to your Github repository - 10 points
 
 # - Paste the Github link of the script in answer box. - 10 points
-if ["${UID}" -ne 0]
+
+if [ "${UID}" -ne "0" ]
 then 
-	echo 'not root user, use sudo'
-	exit 1
+        echo 'not root user, use sudo'
+        exit 1
 fi
 
 for i in {0..1}; do
     read -p "Username: " U
     read -p "Password: " P
-    if [ "${#P}" -le "8" ]
+    if [ "${#P}" -le "7" ]
     then
-        while [ "${#P}" -le "8" ]
+        while [ "${#P}" -le "7" ]
         do
         echo "too short"
         read -p "Password: " P
         done
+        useradd -M ${U}
          echo ${U}
          echo ${P}
     else
+        useradd -M ${U}
          echo ${U}
          echo ${P}
     fi
