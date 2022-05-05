0b9978e47e042e9b1ab630ca567f9bc0ee9c64de
ALilalee <ali.lalee@yahoo.com>
2022-05-05 08:00:58 -0400

    up

--- ITP270bashmid.sh
+++ ITP270bashmid.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
+if [ "${UID}" -ne "0" ]
+then 
+	echo 'not root user, use sudo'
+	exit 1
+fi
 
-
-# - That creates 2 users (Use a for loop to repeat the add user process twice ) - 20 points
-
-# - Takes 2 inputs (Username and password) - 20 points
-
-# - Validate the password so it is at least 8 characters long (use a while loop and If-else condition) - 30 points
-
-# - Print the new user's username and password in the terminal - 10 points
-
-# - Upload the script in to your Github repository - 10 points
-
-# - Paste the Github link of the script in answer box. - 10 points
-
-for i in 0{1..2}
-do
-    read -p "Username: " username 
-    read -p "Password: " password
-    if ["${password}" >= 8]
-    echo "username"
-    echo "password"
-    
+for i in {0..1}; do
+    read -p "Username: " U
+    read -p "Password: " P
+    if [ "${#P}" -le "7" ]
+    then
+        while [ "${#P}" -le "7" ]
+        do
+        echo "too short"
+        read -p "Password: " P
+        done
+	useradd -M ${U}
+         echo ${U}
+         echo ${P}
+    else
+	useradd -M ${U}
+         echo ${U}
+         echo ${P}
+    fi
+ done  
